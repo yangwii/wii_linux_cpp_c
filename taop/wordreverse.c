66664bbcd18e0cbeadfec8910ae98a8764494012
yangpeng <591995637@qq.com>
2014-06-21 18:30:44 +0800

    Update wordreverse.c

--- taop/wordreverse.c
+++ taop/wordreverse.c
@@ -30,10 +30,14 @@ void wordreverse(char *s)
 	char *p = s;
 	int j = 0;
 	int len = strlen(s);
-	reversestr(s, 0, len - 1);
+	reversestr(s, 0, len - 1);//先将整个字符串反转
 	printf("%s\n", s);
+	
+	//调整字符串
 	s[len] = ' ';
 	s[len + 1] = '\0';
+	
+	//每当遇到一个空格时，就将这个单词反转
 	for(int i = 0, j = i; i < strlen(s); i++)
 	{
 		if(s[i] == ' ')
@@ -42,5 +46,7 @@ void wordreverse(char *s)
 			j = i+1;
 		}
 	}
+	
+	//将字符串恢复
 	s[len] = '\0';
 }
