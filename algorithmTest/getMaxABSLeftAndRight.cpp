999ec0188f1d46e9864ed9bd62d1f7afc644ad52
unknown <YANGPENG@YANGPENG.sogou-inc.com>
2015-03-20 17:45:07 +0800

    Committer: unknown <YANGPENG@YANGPENG.sogou-inc.com> 	renamed:    algorithmTest/etMaxABSLeftAndRight.cpp -> algorithmTest/getMaxABSLeftAndRight.cpp

--- algorithmTest/getMaxABSLeftAndRight.cpp
+++ algorithmTest/getMaxABSLeftAndRight.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+/**
+*  求左部分中的最大值减去右部分最大值的绝对值
+*  vec: 输入数组
+*  len vec的长度
+*  返回：左部分中的最大值减去右部分最大值的绝对值
+*/
+ 
 int getMaxABSLeftAndRight(vector<int> vec, int len) {
 	int tail = len - 1;
 	int head = 0;
